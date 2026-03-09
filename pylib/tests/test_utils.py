47d6e62d15924f3b2e2e778bb950d852005e43bf
Brian Le <brian@brianle.xyz>
2026-03-09 03:42:23 -0400

    fix(version): ignore fork suffix in int version

--- pylib/tests/test_utils.py
+++ pylib/tests/test_utils.py
@@ -1,6 +1,8 @@
 # Copyright: Ankitects Pty Ltd and contributors
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
+from anki import buildinfo
+from anki.utils import int_version
 from anki.utils import int_version_to_str
 
 
@@ -8,3 +10,8 @@ def test_int_version_to_str():
     assert int_version_to_str(23) == "2.1.23"
     assert int_version_to_str(230900) == "23.09"
     assert int_version_to_str(230901) == "23.09.1"
+
+
+def test_int_version_ignores_suffixes(monkeypatch):
+    monkeypatch.setattr(buildinfo, "version", "25.09.2-0xble.1.0.0")
+    assert int_version() == 250902
